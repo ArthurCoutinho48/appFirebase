cf1aca41e6b74b7aaaa35d5a252c27ca77b0540b
ArthurCoutinho48 <165350241+ArthurCoutinho48@users.noreply.github.com>
2025-05-22 17:32:45 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appFirebase',
+  webDir: 'www'
+};
+
+export default config;
